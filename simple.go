316ba91bb01f569dadf1c52e36b2894852d6c0ce
Muhammad Ahmad Khan <ahmadkhan.cui@gmail.com>
2022-04-22 12:38:13 +0500

    Code Commit !

--- simple.go
+++ simple.go
@@ -6,12 +6,16 @@ import (
 
 func main() {
 	// Declaration of Variables & Printing them
-	var name string = "Ahmad"  // Type Explicitly Defined
-	var number int = 10        // Type Explicitly Defined
-	var name2 string = "Ahmad" // Type Implicitly Defined
-	var number2 int = 10       // Type Implicitly Defined
+	var name string = "Ahmad" // Type Explicitly Defined
+	var number int = 10       // Type Explicitly Defined
+	var name2 = "Ahmad"       // Type Implicitly Defined
+	var number2 = 10          // Type Implicitly Defined
+	name3 := "Ahmad"          // More Professional
+	number3 := 10             // More Professional
 	number++
 	number2++
+	number3++
 	fmt.Println("Hello", name, number)
 	fmt.Println("Hello", name2, number2)
+	fmt.Println("Hello", name3, number3)
 }
