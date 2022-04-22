a6449b9c60bcecbead31f44f5599eee20bf926e1
Muhammad Ahmad Khan <ahmadkhan.cui@gmail.com>
2022-04-22 12:27:10 +0500

    Code Commit !

--- simple.go
+++ simple.go
@@ -5,5 +5,9 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World")
+	// Declaration of Variables & Printing them
+	var name string = "Ahmad" // Type Explicitly Defined
+	var number int = 10       // Type Explicitly Defined
+	number++
+	fmt.Println("Hello", name, number)
 }
